12504fc01cbd4237312dbc43b719f2202f1f1024
Shawn Koong <shawnkoong@gmail.com>
2023-06-28 19:50:58 -0700

    commenting out @component since station no longer needed

--- main-app/src/main/java/com/nyctransittracker/mainapp/util/StationFetcher.java
+++ main-app/src/main/java/com/nyctransittracker/mainapp/util/StationFetcher.java
@@ -2,22 +2,23 @@ package com.nyctransittracker.mainapp.util;
 
 import com.nyctransittracker.mainapp.model.Stop;
 import com.nyctransittracker.mainapp.model.StationResponse;
+import com.nyctransittracker.mainapp.service.RouteSubscriptionService;
 import com.nyctransittracker.mainapp.service.StopService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.stereotype.Component;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
 import java.io.IOException;
 import java.net.URL;
 import java.util.List;
 
-@Component
+//@Component
 @RequiredArgsConstructor
 public class StationFetcher implements CommandLineRunner {
 
     private final static String url = "https://www.goodservice.io/api/stops/";
-    private final StopService service;
+    private final StopService stopService;
+    private final RouteSubscriptionService routeSubscriptionService;
 
     private static final ObjectMapper mapper = new ObjectMapper();
 
@@ -29,12 +30,10 @@ public class StationFetcher implements CommandLineRunner {
     public void fetchStations() throws IOException {
         StationResponse stationResponse = mapper.readValue(new URL(url), StationResponse.class);
         List<Stop> stops = stationResponse.getStops();
-        stops.forEach(stop ->
-                stop.getRoutes().forEach((route, dList) ->
-                            dList.forEach(d ->
-                                    service.createStop(stop, d.toString(), route.toString())
-                        )
-                )
-        );
+        stops.forEach(stop -> {
+            stop.getRoutes().forEach((route, dList) -> {
+                dList.forEach(d -> stopService.createStop(stop, d.toString(), route));
+            });
+        });
     }
 }
