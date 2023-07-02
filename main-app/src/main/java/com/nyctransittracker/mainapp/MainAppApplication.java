8de4830a3aed8d9f2237d0121809e31312a4cbf3
Shawn Koong <shawnkoong@gmail.com>
2023-07-01 21:44:10 -0700

    formatting

--- main-app/src/main/java/com/nyctransittracker/mainapp/MainAppApplication.java
+++ main-app/src/main/java/com/nyctransittracker/mainapp/MainAppApplication.java
@@ -1,8 +1,6 @@
 package com.nyctransittracker.mainapp;
 
 import com.nyctransittracker.mainapp.config.RedisProperties;
-import com.nyctransittracker.mainapp.util.StationFetcher;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
@@ -11,9 +9,9 @@ import org.springframework.boot.context.properties.EnableConfigurationProperties
 @EnableConfigurationProperties(RedisProperties.class)
 public class MainAppApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(MainAppApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(MainAppApplication.class, args);
+    }
 
 }
 
