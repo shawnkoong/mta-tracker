ff41eb7fad85c7b51de8176549dd23152c36889b
Shawn Koong <shawnkoong@gmail.com>
2023-08-02 15:40:42 -0700

    removing async/threads completely

--- main-app/src/main/java/com/nyctransittracker/mainapp/util/DataFetcher.java
+++ main-app/src/main/java/com/nyctransittracker/mainapp/util/DataFetcher.java
@@ -7,14 +7,12 @@ import com.nyctransittracker.mainapp.service.TimeService;
 import com.nyctransittracker.mainapp.service.TrainPositionService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.scheduling.annotation.Async;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.client.WebClient;
 
 import java.time.Instant;
-import java.util.concurrent.CompletableFuture;
 
 @Component
 @EnableScheduling
@@ -29,9 +27,8 @@ public class DataFetcher {
     private final TimeService timeService;
     private final NotificationService notificationService;
 
-    @Async
     @Scheduled(fixedRate = 15000)
-    public CompletableFuture<Void> fetchData() {
+    public void fetchData() {
         log.info("Starting fetching data: " + Instant.now().toString());
         MtaResponse mtaResponse = webClient.get()
                 .uri(url)
@@ -41,12 +38,8 @@ public class DataFetcher {
         redisService.saveData(mtaResponse);
         log.info("Done with fetching data: " + Instant.now().toString());
 
-        CompletableFuture<Void> processTrainPositionFuture =
-                CompletableFuture.runAsync(trainPositionService::processTrainPositions);
-        CompletableFuture<Void> processArrivalTimesFuture =
-                CompletableFuture.runAsync(timeService::processTimeInfo);
-//        CompletableFuture<Void> processNotification =
-//                CompletableFuture.runAsync(notificationService::testNotification);
-        return CompletableFuture.allOf(processTrainPositionFuture, processArrivalTimesFuture);
+        trainPositionService.processTrainPositions();
+        timeService.processTimeInfo();
+//        notificationService.processNotifications();
     }
 }
