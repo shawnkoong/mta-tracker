1f6fcbe5437aab4212184b40cb7ab833d4d8de0d
Shawn Koong <shawnkoong@gmail.com>
2023-08-26 17:25:09 -0700

    adding oauth2 resource server & rsa properties

--- main-app/src/main/java/com/nyctransittracker/mainapp/MainAppApplication.java
+++ main-app/src/main/java/com/nyctransittracker/mainapp/MainAppApplication.java
@@ -1,12 +1,13 @@
 package com.nyctransittracker.mainapp;
 
+import com.nyctransittracker.mainapp.config.RSAKeyProperties;
 import com.nyctransittracker.mainapp.config.RedisProperties;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.context.properties.EnableConfigurationProperties;
 
 @SpringBootApplication
-@EnableConfigurationProperties(RedisProperties.class)
+@EnableConfigurationProperties({RedisProperties.class, RSAKeyProperties.class})
 public class MainAppApplication {
 
     public static void main(String[] args) {
